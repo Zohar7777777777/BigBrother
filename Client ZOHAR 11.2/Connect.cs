f630c2dd8e1ee098aadb99c81680816345d8ba14
oOoOo <tchaor.hazan@gmail.com>
2024-04-11 11:29:13 +0300

    add client

---
+++ Client ZOHAR 11.2/Connect.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Connect : Form
+    {
+        private string IPAddress;
+
+        public Connect()
+        {
+            InitializeComponent();
+        }
+
+        private void Connect_Load(object sender, EventArgs e)
+        {
+            btnConnection.Enabled = false;
+            var wifiInterface = NetworkInterface.GetAllNetworkInterfaces()
+                .FirstOrDefault(nic => nic.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && nic.OperationalStatus == OperationalStatus.Up);
+
+            if (wifiInterface != null)
+            {
+                var ipv4Address = wifiInterface.GetIPProperties().UnicastAddresses
+                    .FirstOrDefault(addr => addr.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)?.Address;
+
+                if (ipv4Address != null)
+                {
+                    this.IPAddress = ipv4Address.ToString();
+                    lbIP.Text = "YOUR IP ADDRESS IS: " + this.IPAddress;
+                    btnConnection.Enabled = true;
+                }
+                else
+                {
+                    lbIP.Text = "ERROR MESSAGE";
+                }
+            }
+            else
+            {
+                lbIP.Text = "Wi-Fi adapter not found.";
+            }
+        }
+
+        private void btnConnection_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.IPAddress))
+            {
+                MessageBox.Show("No IP address available.");
+                return;
+            }
+
+            Crud crud = new Crud();
+            crud.SetData(tbName.Text, this.IPAddress, 1729);
+            new SharedScreen(this.IPAddress, 1729).Show();
+        }
+
+        private void Disconnect(object sender, FormClosingEventArgs e)
+        {
+            //delete from firebase the user details
+        }
+    }
+}
---
+++ Client ZOHAR 11.2/SharedScreen.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class SharedScreen : Form
+    {
+        private readonly int port;
+        private TcpClient client;
+        private TcpListener server;
+        private NetworkStream mainStream;
+        private readonly Thread Listening;
+        private readonly Thread GetImage;
+
+
+        public SharedScreen(string ip, int Port)
+        {
+            this.port = Port;
+            this.client = new TcpClient();
+            this.Listening = new Thread(StartListening);
+            this.GetImage = new Thread(ReceiveImage);
+
+            InitializeComponent();
+        }
+
+        private void ReceiveImage()
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            while (client.Connected)
+            {
+                mainStream = client.GetStream();
+                pictureBox1.Image = (Image)binaryFormatter.Deserialize(mainStream);
+
+            }
+
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            server = new TcpListener(IPAddress.Any, port);
+            Listening.Start();
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            StopListening();
+        }
+
+
+
+        private void StartListening()
+        {
+            while (!client.Connected)
+            {
+                server.Start();
+                client = server.AcceptTcpClient();
+            }
+            GetImage.Start();
+        }
+
+        private void StopListening()
+        {
+            server.Stop();
+            client = null;
+            if (Listening.IsAlive) Listening.Abort();
+            if (GetImage.IsAlive) Listening.Abort();
+
+        }
+
+        private void SharedScreen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
